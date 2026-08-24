f84794a5380a82664fccef7e901acab7a2b2831a
Qry <juxwaynezqryb@gmail.com>
2026-08-24 23:18:18 +0800

    Added code discussion for Logical, Ternary, Unary Operator

--- src/com/joysistvi/stage1/day5/Activity3_Bergonia.java
+++ src/com/joysistvi/stage1/day5/Activity3_Bergonia.java
@@ -4,13 +4,36 @@ public class Activity3_Bergonia {
 
     public static void main(String[] args) {
 
+        // Task 2: Allowed to enter the event : Yes / No
+
         int age = 18;
         boolean hasValidid = false;
-
         String allowed = age >= 18 && hasValidid ? "Yes" : "No";
         System.out.println("Allowed to enter the event: " + allowed);
 
-        // Allowed to enter the event : Yes / No
+        // Task 3: Class Status check
+        boolean isWeekend = true;
+        boolean isHoliday = false;
+        String noClasses = isWeekend || isHoliday ? "Yes, no classes" : "No classes";
+        System.out.println("Are there no classes today? " + noClasses);
+
+        // Task 4: Pass or Fail Check
+        int score = 82;
+        String result = (score >= 75) ? "Passed" : "Failed";
+        System.out.println("Student Result: " + score + " " + result );
+
+        // Task 5: Larger Number Check
+        int num = 45;
+        int num2 = 67;
+        int larger = (num > num2) ? num : num2;
+        System.out.println("Larger number: " + larger);
+
+
+
+
+
+
+
 
 
     }
